345b22e43365216686f5726e89cec948f8bb84f4
Yair <eljuriyair@gmail.com>
2026-09-24 08:50:27 -0300

    agrego archivo para que los integrantes lo vean

---
+++ for6y7.py
@@ -0,0 +1,5 @@
+#Crea una función que verifique si un número dado es par o impar. La función debe imprimir un mensaje indicando si el número es par o 
+
+#Escribir una función que calcule el área de un rectángulo. La función recibe la base y la altura y retorna el área. 
+def verificar(numero)
+    if 
